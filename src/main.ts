97db79d93fe5ee465048dc3537cb5749eabe10b4
gofixgo <66651502+gofixgo@users.noreply.github.com>
2022-12-24 16:14:52 +0330

    refactor

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(
     cors({
-      origin: ['http://localhost:4201', 'http://localhost:8081', 'http://localhost:3000'],
+      origin: ['http://localhost:4200', 'http://localhost:4201', 'http://localhost:8081', 'http://localhost:3000'],
       credentials: true,
     }),
   );
